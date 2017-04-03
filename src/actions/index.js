7837d94bce4808a7dcf367332f453457e5fb858d
Gmes23 <gmesda23@gmail.com>
2017-04-02 20:51:48 -0400

    changed to https

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '3920c003ff56d4ba4e1eabc4beafeefe';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
